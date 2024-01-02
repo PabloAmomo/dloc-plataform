150b36a09966ccd8ebd6d86d5aa2a2e15882ab8d
Pablo amomo <pabloamomo@gmail.com>
2024-01-02 09:09:09 +0100

    .

--- dloc-api/source/persistence/mySql/getPositions.ts
+++ dloc-api/source/persistence/mySql/getPositions.ts
@@ -6,11 +6,16 @@ import { GetPositionsResult } from "../models/GetPositionsResult";
 const connectionConfig: ConnectionConfig = mySqlConnectionConfig;
 
 const getPositions = async (imei: string, interval: number) : Promise<GetPositionsResult> => { 
-  const params: any[] = [imei];
+  const params: any[] = [imei, imei, interval ?? 1440];
 
-  const sql = `SELECT dateTimeUTC, lat, lng, speed, directionAngle, gsmSignal, batteryLevel
-               FROM \`position\` WHERE imei = ? ORDER BY dateTimeUTC DESC, id DESC LIMIT 50;`;
-               
+  const sql = `
+                (SELECT dateTimeUTC, lat, lng, speed, directionAngle, gsmSignal, batteryLevel
+                  FROM \`position\` WHERE imei = ? ORDER BY dateTimeUTC DESC, id DESC LIMIT 1)
+                UNION
+                (SELECT dateTimeUTC, lat, lng, speed, directionAngle, gsmSignal, batteryLevel
+                    FROM \`position\` WHERE imei = ? AND dateTimeUTC >= DATE_ADD(NOW(), INTERVAL ? SECOND) ORDER BY dateTimeUTC DESC, id DESC);
+    `;
+
   /** Execute query */
   const response: GetPositionsResult = await mySqlQueryAsync(connectionConfig, sql, params).then((response) => {
     /** Check for errors */
