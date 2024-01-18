80169a31499c3ffe350710d7dbf8503158f081f8
Pablo amomo <pabloamomo@gmail.com>
2024-01-18 22:03:20 +0100

    .

--- dloc-frontend/src/App.tsx
+++ dloc-frontend/src/App.tsx
@@ -1,9 +1,18 @@
-import RoutesApp from 'routesApp/RoutesApp';
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import { Device } from 'models/Device';
+import { GetDevicesResult } from 'models/GetDevicesResult';
+import { GetUserResult } from 'models/GetUserResult';
+import { initReactI18next, useTranslation } from 'react-i18next';
+import { useDevicesContext } from 'context/DevicesProvider';
+import { useEffect, useRef } from 'react';
+import { useSnackContext } from 'context/SnackProvider';
+import { useUserContext } from 'context/UserProvider';
+import getDevices from 'services/getDevices/getDevices';
+import getUser from 'services/getUser/getUser';
+import i18n from 'i18next';
 import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
-import lngEN from "languages/en.json";
-import lngES from "languages/es.json";
+import lngEN from 'languages/en.json';
+import lngES from 'languages/es.json';
+import RoutesApp from 'routesApp/RoutesApp';
 
 i18n
   .use(I18nextBrowserLanguageDetector)
@@ -11,10 +20,10 @@ i18n
   .init({
     resources: {
       en: { translation: { ...lngEN } },
-      es: { translation: { ...lngES } }
+      es: { translation: { ...lngES } },
     },
-    lng: "es",
-    fallbackLng: ["es", "en"],
+    lng: 'es',
+    fallbackLng: ['es', 'en'],
     interpolation: { escapeValue: false },
 
     detection: {
@@ -30,10 +39,53 @@ i18n
       cookieMinutes: 10,
       cookieDomain: 'myDomain',
       htmlTag: document.documentElement,
-      cookieOptions: { path: '/', sameSite: 'strict' }
-    }
+      cookieOptions: { path: '/', sameSite: 'strict' },
+    },
   });
 
-function App() { return (<RoutesApp />); }
+function App() {
+  const { setUser } = useUserContext();
+  const { setDevices } = useDevicesContext();
+  const { addSnackbar } = useSnackContext();
+  const { t } = useTranslation();
+  const abortAxios = useRef<AbortController>();
+
+  /** Get devices and user data from API (First Load) */
+  useEffect(() => {
+    console.log('RoutesApp: useEffect: getDevices');
+    getDevices({}, (response: GetDevicesResult) => {
+      try {
+
+        // TODO: Add error handler if response has error...
+        if (response?.error || response.devices?.length === 0) throw new Error(response?.error?.message ?? t('errors.noDevicesReceived'));
+
+        // Convert string params to paramr object and save it in context...
+        let devices: Device[] = response.devices.map((item: Device) => ({ ...item, params: JSON.parse(item.params as any) }));
+ 
+        console.log('RoutesApp: useEffect: getDevices: response', devices);
+
+        setDevices(devices);
+
+      } catch (error: any) {
+        addSnackbar('error', error.message);
+      }
+    },
+    abortAxios);
+
+    getUser({}, (response: GetUserResult) => {
+      try {
+        // TODO: Add error handler if response has error...
+        if (!response || response?.error) throw new Error(response?.error?.message ?? t('errors.noUserDataReceived'));
+
+        setUser(response.user);
+      } catch (error: any) {
+        addSnackbar('error', error.message);
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <RoutesApp />;
+}
 
 export default App;
--- dloc-frontend/src/routesApp/RoutesApp.tsx
+++ dloc-frontend/src/routesApp/RoutesApp.tsx
@@ -1,60 +1,9 @@
-import { Device } from 'models/Device';
-import { GetDevicesResult } from 'models/GetDevicesResult';
-import { GetUserResult } from 'models/GetUserResult';
 import { Routes, Route } from 'react-router-dom';
-import { useDevicesContext } from 'context/DevicesProvider';
-import { useEffect, useRef } from 'react';
-import { useSnackContext } from 'context/SnackProvider';
-import { useTranslation } from 'react-i18next';
-import { useUserContext } from 'context/UserProvider';
-import getDevices from 'services/getDevices/getDevices';
-import getUser from 'services/getUser/getUser';
 import Home from 'pages/Home/Home';
 import Layout from 'templates/Layout';
 import Page404 from 'pages/Page404/Page404';
 
 function RoutesApp() {
-  const { setUser } = useUserContext();
-  const { setDevices } = useDevicesContext();
-  const { addSnackbar } = useSnackContext();
-  const { t } = useTranslation();
-  const abortAxios = useRef<AbortController>();
-
-  /** Get devices and user data from API (First Load) */
-  useEffect(() => {
-    console.log('RoutesApp: useEffect: getDevices');
-    getDevices({}, (response: GetDevicesResult) => {
-      try {
-
-        // TODO: Add error handler if response has error...
-        if (response?.error || response.devices?.length === 0) throw new Error(response?.error?.message ?? t('errors.noDevicesReceived'));
-
-        // Convert string params to paramr object and save it in context...
-        let devices: Device[] = response.devices.map((item: Device) => ({ ...item, params: JSON.parse(item.params as any) }));
- 
-        console.log('RoutesApp: useEffect: getDevices: response', devices);
-
-        setDevices(devices);
-
-      } catch (error: any) {
-        addSnackbar('error', error.message);
-      }
-    },
-    abortAxios);
-
-    getUser({}, (response: GetUserResult) => {
-      try {
-        // TODO: Add error handler if response has error...
-        if (!response || response?.error) throw new Error(response?.error?.message ?? t('errors.noUserDataReceived'));
-
-        setUser(response.user);
-      } catch (error: any) {
-        addSnackbar('error', error.message);
-      }
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
