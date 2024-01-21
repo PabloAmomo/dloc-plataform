46fcbe891e264bac24d6b6891ea71f07fc6d1711
Pablo amomo <pabloamomo@gmail.com>
2024-01-21 11:02:37 +0100

    Refactor processMapPaths function to improve performance and readability

--- dloc-frontend/src/functions/processMapPaths.ts
+++ dloc-frontend/src/functions/processMapPaths.ts
@@ -1,3 +1,4 @@
+import { config } from 'config/config';
 import { Device } from 'models/Device';
 import { LatLng } from 'models/LatLng';
 import { Location } from 'models/Location';
@@ -5,7 +6,6 @@ import { MapPath } from 'models/MapPath';
 import { Path } from 'models/Path';
 import convertUTCDateToLocalDate from './convertUTCDateToLocalDate';
 import getDistanceFromLatLonInMeters from './getDistanceFromLatLonInMeters';
-import { config } from 'config/config';
 
 const processMapPaths = (devices: Device[], mapPaths: MapPath[]) => {
   const newMapPaths = [...(mapPaths ?? [])];
@@ -22,67 +22,46 @@ const processMapPaths = (devices: Device[], mapPaths: MapPath[]) => {
       params: { pathColor: color },
     } = device;
 
-    /** Find device index */
+    /** Find device index and add a new device if not exist, and select it */
     let index = newMapPaths.findIndex((mapPath: MapPath) => mapPath.imei === imei);
-
-    /** Not found - Add the new device */
     if (index === -1) {
       const lastPosistion: LatLng = { lat, lng };
       newMapPaths.push({ imei, lastPositionUTC, path: [], lastPosistion, color, strokeWeight: 3, strokeOpacity: 0.25, distance: 0 });
       index = newMapPaths.length - 1;
     }
-
-    /** Process locations for each device */
     const mapPath = newMapPaths[index];
-    for (let i = 0; i < locations.length; i++) {
-      const location: Location = locations[i];
-      const dateTimeUTCms: number = convertUTCDateToLocalDate(location.dateTimeUTC).getTime();
-      const existIndex: number = mapPath.path.findIndex((path) => path.dateTimeUTC === location.dateTimeUTC);
-      const lastPosition: LatLng = {
-        lat: mapPath?.path?.[mapPath.path.length - 1]?.end?.lat ?? mapPath.lastPosistion.lat,
-        lng: mapPath?.path?.[mapPath.path.length - 1]?.end?.lng ?? mapPath.lastPosistion.lng,
-      };
 
-      /** Location already exist, go for next */
-      if (existIndex !== -1) continue;
+    /** Create paths, merging currents paths and new locations */
+    const paths: Map<string, Path> = new Map<string, Path>();
+    mapPath.path.forEach((path: Path) => paths.set(path.dateTimeUTC, path));
+    locations.forEach((location: Location) => {
+      const locTemp: LatLng = { lat: location.lat, lng: location.lng };
+      const dateTimeUTC: string = location.dateTimeUTC;
+      paths.set(dateTimeUTC, { start: { ...locTemp }, end: { ...locTemp }, dateTimeUTC });
+    });
 
-      /** Find index where to insert new path */
-      let insertInto = mapPath.path.findIndex((path) => dateTimeUTCms < convertUTCDateToLocalDate(path.dateTimeUTC).getTime());
-
-      /** Start and end lat and lng position */
-      const start: LatLng = insertInto > 0 ? { lat: mapPath.path[insertInto - 1].end.lat, lng: mapPath.path[insertInto - 1].end.lng } : lastPosition;
-      const end: LatLng = { lat: location.lat, lng: location.lng };
-
-      /** Update the start lat and lng in the next path */
-      if (insertInto > 0) mapPath.path[insertInto - 1].end = { ...start };
-
-      /** Update the start lat and lng in the next path */
-      if (insertInto !== -1) mapPath.path[insertInto].start = { ...end };
-
-      /** Add new path in the correct index */
-      if (insertInto === -1) mapPath.path.push({ start, end, dateTimeUTC: location.dateTimeUTC });
-      else mapPath.path.splice(insertInto, 0, { start, end, dateTimeUTC: location.dateTimeUTC });
-
-      /** Calculate distance in meter bettween start and end lat and lng position */
-      const distance = getDistanceFromLatLonInMeters(start.lat, start.lng, end.lat, end.lng);
-      mapPath.distance += distance;
-    }
-
-    if (mapPath.path.length > 0) {
-      /** Update first path */
-      mapPath.path[0].start = { ...mapPath.path[0].end };
-      /** Update last path */
-      const lastPath: Path = mapPath.path[mapPath.path.length - 1];
-      lastPath.end = { ...lastPath.start };
-      mapPath.lastPosistion = { ...lastPath.end };
-      mapPath.lastPositionUTC = lastPath.dateTimeUTC;
-    }
+    /** Make array of paths and sort by dateTimeUTC */
+    const newPaths: Path[] = Array.from(paths, ([name, value]) => value);
+    newPaths.sort((a: Path, b: Path) => convertUTCDateToLocalDate(a.dateTimeUTC).getTime() - convertUTCDateToLocalDate(b.dateTimeUTC).getTime());
 
     /** Max path length, remove execces paths */
-    while (mapPath.path.length > config.map.maxPathsByDevice) {
-      const removed: Path | undefined = mapPath.path.shift();
-      if (removed) mapPath.distance -= getDistanceFromLatLonInMeters(removed.start.lat, removed.start.lng, removed.end.lat, removed.end.lng);
+    while (newPaths.length > config.map.maxPathsByDevice) {
+      newPaths.shift();
     }
+
+    /** Correct paths to start and end correctly */
+    mapPath.distance = 0;
+    newPaths.forEach((path: Path, index: number) => {
+      if (index === 0) path.start = { ...path.end };
+      else {
+        const prevPath: Path = newPaths[index - 1];
+        prevPath.end = { ...path.start };
+        mapPath.distance += getDistanceFromLatLonInMeters(prevPath.start.lat, prevPath.start.lng, prevPath.end.lat, prevPath.end.lng)
+      };
+    });
+
+    /** Update paths */
+    mapPath.path = newPaths;
   }
 
   /** Return new mapPaths */
